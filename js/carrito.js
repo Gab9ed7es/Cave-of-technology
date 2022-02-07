a80552e5060d59da66858814f2d7b794bab52f11
Gabriel Escobar <gabi180020@gmail.com>
2022-02-07 17:02:28 -0300

    Actualización

--- js/carrito.js
+++ js/carrito.js
@@ -17,7 +17,25 @@
             id : producto.querySelector('a').getAttribute('data-id'),
             cantidad : 1
         }
-        this.insertarCarrito(infoProducto);
+        let productosLS;
+        productosLS = this.obtenerProductosLocalStorage();
+        productosLS.forEach(function(productoLS){
+            if(productoLS.id === infoProducto.id){
+                productosLS = productoLS.id
+            }
+        });
+        if(productosLS === infoProducto.id){
+            Swal.fire({
+                type: 'info',
+                title: 'Oops...',
+                text: 'El producto ya fue agregado',
+                timer: 2000,
+                showConfirmButton: false
+              })
+        }
+        else{
+            this.insertarCarrito(infoProducto);
+        }
     }
 
     insertarCarrito(producto){
@@ -33,6 +51,7 @@
         </td>
         `;
         listaProductos.appendChild(row);
+        this.guardarProductosLocalStorage(producto);
     }
     
     eliminarProducto(e){
@@ -43,6 +62,8 @@
             producto = e.target.parentElement.parentElement;
             productoID = producto.querySelector('a').getAttribute('data-id');
         }
+        this.eliminarProductoLocalStorage(productoID);
+        this.calcularTotal();
     }
 
     vaciarCarrito(e){
@@ -50,6 +71,119 @@
         while(listaProductos.firstChild){
             listaProductos.removeChild(listaProductos.firstChild);
         }
+        this.vaciarLocalStorage();
         return false;
     }
+
+
+    guardarProductosLocalStorage(producto){
+        let productos;
+        productos = this.obtenerProductosLocalStorage();
+        productos.push(producto); 
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+
+    obtenerProductosLocalStorage(){
+        let productoLS;
+
+        if(localStorage.getItem('productos') === null){
+            productoLS = [];
+        }
+        else {
+            productoLS = JSON.parse(localStorage.getItem('productos'));
+        }
+        return productoLS
+    }
+
+    eliminarProductoLocalStorage(productoID){
+        let productosLS;
+        productosLS = this.obtenerProductosLocalStorage();
+        productosLS.forEach(function(productoLS, index){
+            if(productoLS.id === productoID){
+                productosLS.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('productos', JSON.stringify(productosLS));
+    }
+
+    leerLocalStorage(){
+        let productosLS;
+        productosLS = this.obtenerProductosLocalStorage();
+        productosLS.forEach(function(producto){
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>
+                    <img src="${producto.imagen}" width=100>
+                </td>
+                <td>${producto.titulo}</td>
+                <td>${producto.precio}</td>
+                <td>
+                    <a href="#" class="borrar-producto fas fa-times-circle" data-id="${producto.id}"></a>
+                </td>
+            `;
+        listaProductos.appendChild(row);
+        });
+    }
+
+    leerLocalStorageCompra(){
+        let productosLS;
+        productosLS = this.obtenerProductosLocalStorage();
+        productosLS.forEach(function(producto){
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>
+                    <img src="${producto.imagen}" width=100>
+                </td>
+                <td>${producto.titulo}</td>
+                <td>${producto.precio}</td>
+                <td>
+                    <input type="number" class="form-control cantidad" min="1" value= ${producto.cantidad}>
+                </td>
+                <td>${producto.precio * producto.cantidad}</td>
+                <td>
+                    <a href="#" class="borrar-producto fas fa-times-circle" style="font-size:30px" data-id="${producto.id}"></a>
+                </td>
+            `;
+        listaCompra.appendChild(row);
+        });
+    }
+
+    vaciarLocalStorage(){
+        localStorage.clear();
+    }
+    
+    procesarPedido(e){
+        e.preventDefault();
+        if(this.obtenerProductosLocalStorage().length === 0){
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'El carrito esta vacío',
+                timer: 3000,
+                showConfirmButton: false
+              })
+        }
+        else{
+            location.href = "compra.html"
+        }
+    }
+
+    calcularTotal(){
+        let productoLS;
+        let total = 0, subTotal = 0, iva = 0;
+        productoLS = this.obtenerProductosLocalStorage();
+        for (let i = 0; i < productoLS.length; i++){
+            let element = Number(productoLS[i].precio * productoLS[i].cantidad);
+            total= total + element;
+        }
+        iva = parseFloat(total * 0.21).toFixed(2);
+        subTotal = parseFloat(total-iva).toFixed(2);
+
+        document.getElementById('subtotal').innerHTML= "$ " + subTotal;
+        document.getElementById('iva').innerHTML= "$ " + iva;
+        document.getElementById('total').innerHTML= "$ " + total.toFixed(2);
+    }
+
+
 }
